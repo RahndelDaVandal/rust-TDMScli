57bf7dfe49103efa4838201c410c0cf1059a3f80
Connor Sahleen <Connor.Sahleen@gmail.com>
2022-08-03 13:54:00 -0500

    syncing progress

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
+use std::io::BufReader;
 use std::path::{Path,PathBuf};
 use std::fs::File;
+use std::io::Read;
 
 fn main() {
     let mut file_path = PathBuf::new();
@@ -16,5 +18,37 @@ fn main() {
         }
     }
     dbg!(&file_path, &file_path.exists());
+
+    let mut file = File::open(file_path).expect("Error reading file");
+    // let mut handle = file.take(4);
+    // let mut buffer = [0; 4];
+
+    // handle.read(&mut buffer).expect("err reading handle");
+
+    // let s = String::from_utf8_lossy(&mut buffer);
+    // println!("{}", s);
+    let buffer = read_4bytes(&mut file);
+    println!("{:?}",String::from_utf8_lossy(&buffer));
+    let buffer = read_4bytes(&mut file);
+    println!("{:?}", buffer);
+    let buffer = read_4bytes(&mut file);
+    println!("{:?}", buffer);
+    let buffer = read_8bytes(&mut file);
+    println!("{:?}", buffer);
+    let buffer = read_8bytes(&mut file);
+    println!("{:?}", buffer);
 }
 
+fn read_4bytes(file: &mut File) -> [u8; 4]{
+    let mut buffer = [0; 4];
+    let mut handle = file.take(4);
+    handle.read(&mut buffer).expect("error reading bytes");
+    return buffer
+}
+
+fn read_8bytes(file: &mut File) -> [u8; 8]{
+    let mut buffer = [0; 8];
+    let mut handle = file.take(8);
+    handle.read(&mut buffer).expect("error reading bytes");
+    return buffer
+}
