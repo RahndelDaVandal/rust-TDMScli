3100d243c6d2f359a925959fbff6ee51b5f9ce36
Connor Sahleen <Connor.Sahleen@gmail.com>
2022-08-05 22:20:32 -0500

    trying to figure out how to read bytes into an unditermined length array. Got it to work with vectors but then converting vector to u32 doesn't work because it wants [u8; 4]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::env;
 use std::io::BufReader;
-use std::path::{Path,PathBuf};
+use std::path::PathBuf;
 use std::fs::File;
 use std::io::Read;
+
 
 fn main() {
     let mut file_path = PathBuf::new();
@@ -19,36 +20,99 @@ fn main() {
     }
     dbg!(&file_path, &file_path.exists());
 
-    let mut file = File::open(file_path).expect("Error reading file");
-    // let mut handle = file.take(4);
-    // let mut buffer = [0; 4];
+    // let file = File::open(file_path).expect("Error reading file");
+    //
+    // let mut reader = BufReader::new(file);
+    // 
+    // let mut tag = [0; 4];
+    // read_bytes(&mut reader, &mut tag);
+    // println!("tag: {}", String::from_utf8_lossy(&tag));
+    //
+    // let mut mask = [0; 4];
+    // read_bytes(&mut reader, &mut mask);
+    // println!("{:0<4b} {:0<4b} {:0<4b} {:0<4b}", mask[0], mask[1], mask[2], mask[3]);
+    //
+    // let mut ver = [0; 4];
+    // read_bytes(&mut reader, &mut ver);
+    // println!("ver: {:?}", u32::from_le_bytes(ver));
+    //
+    // let mut seg = [0; 8];
+    // read_bytes(&mut reader, &mut seg);
+    // println!("seg: {}", u64::from_le_bytes(seg));
+    //
+    // let mut raw = [0; 8];
+    // read_bytes(&mut reader, &mut raw);
+    // println!("raw: {}", u64::from_le_bytes(raw));
 
-    // handle.read(&mut buffer).expect("err reading handle");
-
-    // let s = String::from_utf8_lossy(&mut buffer);
-    // println!("{}", s);
-    let buffer = read_4bytes(&mut file);
-    println!("{:?}",String::from_utf8_lossy(&buffer));
-    let buffer = read_4bytes(&mut file);
-    println!("{:?}", buffer);
-    let buffer = read_4bytes(&mut file);
-    println!("{:?}", buffer);
-    let buffer = read_8bytes(&mut file);
-    println!("{:?}", buffer);
-    let buffer = read_8bytes(&mut file);
-    println!("{:?}", buffer);
+    let mut r = Reader::new(file_path);
+    let tag = r.read_next(4);
+    dbg!(String::from_utf8_lossy(&tag));
+    let mask = r.read_next(4);
+    dbg!(mask);
+    let mut ver = r.read_next(4);
+    dbg!(&ver);
 }
 
-fn read_4bytes(file: &mut File) -> [u8; 4]{
-    let mut buffer = [0; 4];
-    let mut handle = file.take(4);
-    handle.read(&mut buffer).expect("error reading bytes");
-    return buffer
+#[derive(Debug)]
+struct Reader{
+    reader: BufReader<File>,
 }
 
-fn read_8bytes(file: &mut File) -> [u8; 8]{
-    let mut buffer = [0; 8];
-    let mut handle = file.take(8);
-    handle.read(&mut buffer).expect("error reading bytes");
-    return buffer
+impl Reader{
+    fn new(file_path: PathBuf) -> Self{
+        let file = File::open(file_path).expect("Error opening file");
+        Self{
+            reader : BufReader::new(file),
+        }
+    }
+    fn read_next(&mut self, num: usize) -> Vec<u8>{
+        let mut buf = vec![0u8; num];
+        match self.reader.read(&mut buf){
+            Ok(_) => {},
+            Err(e) => eprintln!("Error: {}", e),
+        };
+        buf
+    }
 }
+
+#[derive(Debug)]
+struct LeadIn{
+    tag: String,
+    mask: [u8; 4],
+    ver: u32,
+    seg: u64,
+    raw: u64,
+}
+
+impl LeadIn{
+    fn new() -> Self{
+        Self {
+            tag: String::new(),
+            mask: [0; 4],
+            ver:  0,
+            seg: 0,
+            raw: 0,
+        }
+    }
+}
+
+fn read_bytes(reader: &mut BufReader<File>, output: &mut [u8]){
+    match reader.read(output){
+        Ok(_) => {},
+        Err(e) => eprintln!("Error: {}", e)
+    };
+}
+
+// fn read_4bytes(file: &mut File) -> [u8; 4]{
+//     let mut buffer = [0; 4];
+//     let mut handle = file.take(4);
+//     handle.read(&mut buffer).expect("error reading bytes");
+//     return buffer
+// }
+
+// fn read_8bytes(file: &mut File) -> [u8; 8]{
+//     let mut buffer = [0; 8];
+//     let mut handle = file.take(8);
+//     handle.read(&mut buffer).expect("error reading bytes");
+//     return buffer
+// }
